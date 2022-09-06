6e5f524607ef5b6f37079b7b6ef0888a243ef52b
KraS27 <groverkras@gmail.com>
2022-09-06 18:52:23 +0300

    StableVersion

--- Market.Service/Implementations/ProductService.cs
+++ Market.Service/Implementations/ProductService.cs
@@ -8,14 +8,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Market.Service.Implementations
 {
     public class ProductService : IProductService
     {
         private readonly IProductRepository _productRepository;
+        
 
         public ProductService(IProductRepository productRepository)
         {
--- Market/Startup.cs
+++ Market/Startup.cs
@@ -1,20 +1,15 @@
 using Market.DAL;
 using Market.DAL.Interfaces;
 using Market.DAL.Repositories;
+using Market.Domain.Entity;
 using Market.Service.Interfaces;
 using Market.Service.Implementations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Market.Domain.Entity;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Market
